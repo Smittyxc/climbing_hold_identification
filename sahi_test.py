f7d5f2db3a3645f0426b60060395e2c6e21968fd
Matt Smith <mattsmith1652@gmail.com>
2026-03-31 08:31:08 -0400

    added best4.pt

--- sahi_test.py
+++ sahi_test.py
@@ -1,46 +1,52 @@
 from sahi import AutoDetectionModel
 from sahi.predict import get_sliced_prediction
+from PIL import Image
+import time
 
-# 1. Load your locally trained YOLO model into SAHI
-detection_model = AutoDetectionModel.from_pretrained(
-    model_type="ultralytics",
-    model_path=r'C:\Users\matts\winter2026\cis378\proj\best3.pt',
-    confidence_threshold=0.50,
-    device="cpu" 
-)
+data = {
+    'img_name': ['result0', 'result1', 'result2', 'result' ],
+    'true_hold_count': [],
+    'est_hold_count': []
+}
+def sahi_detection_model(save_path, test_paths, conf=0.50):
+    MIN_PIXEL_AREA = 400  
+    detection_model = AutoDetectionModel.from_pretrained(
+        model_type="ultralytics",
+        model_path=r'C:\Users\matts\winter2026\cis378\climbing_hold_identification\best4.pt',
+        confidence_threshold=conf,
+        device="cpu" 
+    )
 
-image_path = r'C:\Users\matts\winter2026\cis378\proj\newwall3.webp'
+    for i, img_path in enumerate(test_paths):
+        start_time_iteration = time.time()
+        width, height = get_image_size(img_path)
 
-# 2. Run the sliced inference!
-# We are slicing the image into 512x512 squares with a 20% overlap 
-# so holds on the edges of the slices don't get cut in half.
-result = get_sliced_prediction(
-    image_path,
-    detection_model,
-    slice_height=512,
-    slice_width=512,
-    overlap_height_ratio=0.2,
-    overlap_width_ratio=0.2,
-)
+        result = get_sliced_prediction(
+            img_path,
+            detection_model,
+            slice_height=height // 2,
+            slice_width=width // 2,
+            overlap_height_ratio=0.4,
+            overlap_width_ratio=0.4,
+        )
+        
+        result.object_prediction_list = filter_large_box(width, height, result.object_prediction_list)
+        result.export_visuals(export_dir=save_path, file_name=f"result{i}", hide_conf=True, hide_labels=True)
+        
+        end_time_iteration = time.time()
+        iteration_time = end_time_iteration - start_time_iteration
+        print(f"Iteration {i+1} took: {iteration_time} seconds")
 
+def get_image_size(path: str):
+    with Image.open(path) as img:
+        return img.size
 
-clean_predictions = []
-MIN_PIXEL_AREA = 400  # Adjust this: 300 pixels is roughly a 17x17 pixel square
+def filter_large_box(width, height, preds): 
+    filtered_preds = []
+    img_size = width * height
+    for pred in preds:
+        box_area = pred.bbox.area
+        if box_area < img_size / 25:
+            filtered_preds.append(pred)
+    return filtered_preds
 
-# Loop through everything SAHI found
-for obj in result.object_prediction_list:
-    # SAHI provides the exact area of the bounding box
-    box_area = obj.bbox.area 
-    
-    if box_area > MIN_PIXEL_AREA:
-        clean_predictions.append(obj)
-    else:
-        print(f"Filtered out tiny bolt hole (Area: {box_area})")
-
-# 4. Overwrite SAHI's raw list with our clean, filtered list
-result.object_prediction_list = clean_predictions
-
-
-# 3. Export the visual result to see how it did
-result.export_visuals(export_dir=r'C:\Users\matts\winter2026\cis378\proj', file_name="sahi_result", hide_conf=True, hide_labels=True)
-print("Done! Check your folder for the sahi_result.png file.")
